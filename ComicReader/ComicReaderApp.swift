e4a24b9de20dd05cbc148e96e7e31797dd67d41f
Tenzin Norden <nordentenzin29@gmail.com>
2023-08-16 11:10:40 +0530

    Initial Commit

---
+++ ComicReader/ComicReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicReaderApp.swift
+//  ComicReader
+//
+//  Created by Tenzin Norden on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ComicReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
